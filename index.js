6df9adb815ba23fce3cbf494455a4edb687c622e
canikwe <c.anikwe@gmail.com>
2019-02-17 13:54:55 -0500

    Work with Map function

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(driver => driver.toLowerCase())
+}
+
+function nameToAttributes(drivers) {
+  return drivers.map(function (driver) {
+    const splitDriver = driver.split(" ")
+    return Object.assign({}, {
+      "firstName": splitDriver[0],
+      "lastName": splitDriver[1]
+    })
+  })
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function (driver) {
+    return `${driver["name"]} is from ${driver["hometown"]}`
+  })
+}
